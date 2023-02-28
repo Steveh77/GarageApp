32a7ca65dd70ea76bedd464bbf269f04abb7bbb3
Steveh77 <stederosa76@gmail.com>
2023-03-01 00:53:28 +0100

    Fixed other recyclerView items space and added spinner

--- app/src/main/java/it/academy/android/garageappfinal20/fragments/AddInterventionFragment.kt
+++ app/src/main/java/it/academy/android/garageappfinal20/fragments/AddInterventionFragment.kt
@@ -5,19 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 import androidx.navigation.fragment.findNavController
 import it.academy.android.garageappfinal20.R
 import it.academy.android.garageappfinal20.database.GarageDatabase
 import it.academy.android.garageappfinal20.entities.Client
 import it.academy.android.garageappfinal20.entities.Intervention
+import it.academy.android.garageappfinal20.entities.Vehicle
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class AddInterventionFragment : Fragment() {
-
+    private lateinit var vehicleSpinner: Spinner
+    private lateinit var adapter: ArrayAdapter<Vehicle>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,10 +39,26 @@ class AddInterventionFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_add_intervention, container, false)
         val database = GarageDatabase.getDatabase(requireContext())
 
+
+        //spinner
+        vehicleSpinner = view.findViewById(R.id.vehicle_spinner)
+        adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        vehicleSpinner.adapter = adapter
+
+        //problema thread
+        GlobalScope.launch {
+            val vehicles = database.vehicleDao().getAll()
+            withContext(Dispatchers.Main) {
+                adapter.addAll(vehicles)
+                adapter.notifyDataSetChanged()
+            }
+        }
+
         // Imposto un onClickListener per il pulsante
         view.findViewById<Button>(R.id.save_intervention_button).setOnClickListener {
 
-            // Ottieni i valori di input per descrizione,ore di lavoro, data di inizio e data di fine
+            // Ottieni i valori di input per descrizione,ore di lavoro, data di inizio , data di fine e ID veicolo
             val descrizione =
                 view.findViewById<EditText>(R.id.new_description_intervention_editText).text.toString()
             val oreDiLavoro =
@@ -47,10 +69,14 @@ class AddInterventionFragment : Fragment() {
             val dataDiFine =
                 view.findViewById<EditText>(R.id.new_endDate_intervention_editText).text.toString()
 
-            // Crea un nuovo oggetto Intervento
-            val intervento = Intervention(0, descrizione, oreDiLavoro, dataDiInizio, dataDiFine, 1)
+            // Prendo l'id del veicolo selezionato nello spinner
+            val vehicleId = (vehicleSpinner.selectedItem as Vehicle).id
 
-            // Inserisco il nuovo oggetto Cliente nel database
+            // Crea un nuovo oggetto Intervento
+            val intervento =
+                Intervention(0, descrizione, oreDiLavoro, dataDiInizio, dataDiFine, vehicleId)
+
+            // Inserisco il nuovo oggetto Veicolo nel database
             GlobalScope.launch {
                 database.interventionDao().save(intervento)
 
--- app/src/main/java/it/academy/android/garageappfinal20/fragments/AddVehicleFragment.kt
+++ app/src/main/java/it/academy/android/garageappfinal20/fragments/AddVehicleFragment.kt
@@ -5,21 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 import androidx.navigation.fragment.findNavController
 import it.academy.android.garageappfinal20.R
 import it.academy.android.garageappfinal20.database.GarageDatabase
 import it.academy.android.garageappfinal20.entities.Client
 import it.academy.android.garageappfinal20.entities.Vehicle
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class AddVehicleFragment : Fragment() {
-
+    //variabili spinner
+    private lateinit var clientSpinner: Spinner
+    private lateinit var adapter: ArrayAdapter<Client>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
     }
 
@@ -28,15 +35,31 @@ class AddVehicleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        // Inflate the layout for this fragment
         // istanza del database
         val view = inflater.inflate(R.layout.fragment_add_vehicle, container, false)
         val database = GarageDatabase.getDatabase(requireContext())
 
-        // Imposto un onClickListener per il pulsante
-        view.findViewById<Button>(R.id.save_vehicle_button).setOnClickListener {
 
-            // Ottieni i valori di input per marca, modello,targa e telaio
+        //spinner
+        clientSpinner = view.findViewById(R.id.client_spinner)
+        adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        clientSpinner.adapter = adapter
+
+        //problema thread
+        GlobalScope.launch {
+            val clients = database.clienteDao().getAll()
+            withContext(Dispatchers.Main) {
+                adapter.addAll(clients)
+                adapter.notifyDataSetChanged()
+            }
+        }
+
+
+        // Imposto un onClickListener per il pulsante
+
+        view.findViewById<Button>(R.id.save_vehicle_button).setOnClickListener {
+            // Ottieni i valori di input per marca, modello,targa e telaio e IDcliente
             val marca = view.findViewById<EditText>(R.id.new_brand_vehicle_editText).text.toString()
             val modello =
                 view.findViewById<EditText>(R.id.new_model_vehicle_editText).text.toString()
@@ -44,21 +67,19 @@ class AddVehicleFragment : Fragment() {
             val telaio =
                 view.findViewById<EditText>(R.id.new_chassis_vehicle_editText).text.toString()
 
-            // Crea un nuovo oggetto veicolo
-            val vehicle = Vehicle(0, marca, modello, targa, telaio, 1)
+            // Prendo l'id del cliente selezionato nello spinner
+            val clientId = (clientSpinner.selectedItem as Client).id
 
-            // Inserisco il nuovo oggetto Cliente nel database
+            val vehicle = Vehicle(0, marca, modello, targa, telaio, clientId)
+
             GlobalScope.launch {
                 database.vehicleDao().save(vehicle)
-
             }
             findNavController().navigate(R.id.action_addVehicleFragment_to_vehicleListFragment)
-
-
         }
+
         return view
-
     }
-
-
 }
+
+
